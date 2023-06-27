68d340b1f017a85c0a0c9e2c051bd2c48049dafc
Kari Salokas <kari.salokas@gmail.com>
2023-06-27 14:08:44 +0300

    temp

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -4,6 +4,6 @@ bind = "0.0.0.0:8050"
 
 workers = multiprocessing.cpu_count() * 2 + 1
 #worker_class = 'async'
-worker_connections = 1000
+#worker_connections = 1000
 timeout = 1200
-spew = False
+#spew = False
