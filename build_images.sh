36fcfc0d44aff3b293fd69810788cea5ceab2956
Kari Salokas <kari.salokas@gmail.com>
2023-08-09 10:13:01 +0300

    Modified docker build

--- build_images.sh
+++ build_images.sh
@@ -12,4 +12,5 @@ mv * ../develop
 cd ../prod
 sudo docker build -t pgtesting:prod -f dockerfile . 
 cd ../develop
+mv dockerfile_testing dockerfile
 sudo docker build -t pgtesting:testing -f dockerfile_testing .
