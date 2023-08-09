d46e8f420a4b7addee08a6253b75fd29ace3618e
Kari Salokas <kari.salokas@gmail.com>
2023-08-09 10:16:36 +0300

    Modified docker build

--- build_images.sh
+++ build_images.sh
@@ -8,9 +8,10 @@ cp -r * ../prod/
 git checkout develop
 git pull
 cp -r * ../develop
+git checkout main
 
 cd ../prod
 sudo docker build -t pgtesting:prod -f dockerfile . 
 cd ../develop
 mv dockerfile_testing dockerfile
-sudo docker build -t pgtesting:testing -f dockerfile_testing .
+sudo docker build -t pgtesting:testing -f dockerfile .
