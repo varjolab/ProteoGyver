83de1adcd87a56b20c5cd0a50c4451e134fd6d01
Kari Salokas <kari.salokas@gmail.com>
2023-09-21 16:32:37 +0300

    mod

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,5 +2,15 @@
 
 # Start dash app
 cd /proteogyver
+redis-cli shutdown
+killall celery
+redis-server --daemonize yes
+#supervisorctl start celery
+celery -A app.celery_app worker --loglevel=DEBUG &
+sleep 15
 echo "starting dash app"
 gunicorn -b 0.0.0.0:8050 app:server --log-level debug --timeout 1200 
+
+
+
+#
