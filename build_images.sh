6a2a525777b2624766c4fdc35cb2e31f31b2b56a
Kari Salokas <kari.salokas@gmail.com>
2023-11-07 12:48:09 +0200

    hi

--- build_images.sh
+++ build_images.sh
@@ -4,12 +4,13 @@ mkdir ../prod
 mkdir ../develop
 git checkout main
 git pull
-cp -r * ../prod/
+
+#cp -r * ../prod/
 git checkout develop
 git pull
 cp -r  * ../develop
 
 cd ../prod
-docker build -t pgtesting:prod -f dockerfile .
+#sudo docker build -t pgtesting:prod -f dockerfile .
 cd ../develop
-docker build -t pgtesting:testing -f dockerfile_testing .
+sudo docker build -t pgtesting:testing -f dockerfile_testing .
