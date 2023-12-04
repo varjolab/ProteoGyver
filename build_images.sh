1ab253e1e311f189b31b897b503a3e2150128257
Kari Salokas <kari.salokas@gmail.com>
2023-12-04 14:55:28 +0200

    Mod

--- build_images.sh
+++ build_images.sh
@@ -1,21 +1,9 @@
 
-
 mkdir ../prod
-mkdir ../develop
-mkdir ../old_version
 git checkout main
 git pull
 cp -r * ../prod/
-git checkout develop
-git pull
-cp -r * ../develop
-git checkout old_version
-git pull
-cp -r * ../old_version
 
 cd ../prod
-sudo docker build -t pgtesting:prod -f dockerfile .
-cd ../develop
-sudo docker build -t pgtesting:testing -f dockerfile .
-cd ../old_version
-sudo docker build -t pgtesting:oldversion -f dockerfile .
+sudo docker build -t pgtesting:1.101 -f dockerfile .
+rm -rf ../prod
