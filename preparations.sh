2c8a1380e922cfc014f39ff1dde70732cd138257
Kari Salokas <kari.salokas@helsinki.fi>
2025-04-23 14:28:22 +0300

    Prep script

---
+++ preparations.sh
@@ -0,0 +1,4 @@
+mkdir -p /data/PG_containers/conf/PG_prod
+mkdir -p /data/PG_containers/db/PG_prod
+mkdir -p /data/PG_containers/cache/PG_prod
+cp app/parameters.toml /data/PG_containers/conf/PG_prod/parameters.toml
