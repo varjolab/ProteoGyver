a2ca893a8bd41d0afb3cb615157daa0e72b61a23
Kari Salokas <kari.salokas@gmail.com>
2025-04-23 14:34:23 +0300

    Added sed from dockerfile to preparations

--- preparations.sh
+++ preparations.sh
@@ -1,4 +1,8 @@
 mkdir -p /data/PG_containers/conf/PG_prod
 mkdir -p /data/PG_containers/db/PG_prod
 mkdir -p /data/PG_containers/cache/PG_prod
+cd app
+sed -i 's\"/home", "kmsaloka", "Documents", "PG_cache"\"/proteogyver", "cache"\g' parameters.toml  
+sed -i 's\"Local debug" = true\"Local debug" = false\g' parameters.toml
+cd ..
 cp app/parameters.toml /data/PG_containers/conf/PG_prod/parameters.toml
