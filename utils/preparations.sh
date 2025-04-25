3b05d5876f1d4a7d1282f649615d2410acbeabb5
Kari Salokas <kari.salokas@gmail.com>
2025-04-25 09:28:13 +0300

    Sed trouble fix

--- utils/preparations.sh
+++ utils/preparations.sh
@@ -5,8 +5,8 @@ mkdir -p /data/PG_containers/api_data/PG_prod
 mkdir -p /data/PG_containers/db/PG_test
 mkdir -p /data/PG_containers/cache/PG_test
 cd app
-sed -i 's\"/home", "kmsaloka", "Documents", "PG_cache"\"/proteogyver", "cache"\g' parameters.toml  
-sed -i 's\"/media/kmsaloka/Expansion/20241118_parse/ms_runs/"/proteogyver/data/Server_input/MS run data"\g' parameters.toml
-sed -i 's\"Local debug" = true\"Local debug" = false\g' parameters.toml
+sed -i 's|"/home", "kmsaloka", "Documents", "PG_cache"|"/proteogyver", "cache"|g' parameters.toml
+sed -i 's|"/media/kmsaloka/Expansion/20241118_parse/ms_runs/"|"/proteogyver/data/Server_input/MS run data"|g' parameters.toml
+sed -i 's|Local debug" = true|Local debug" = false|g' parameters.toml
 cd ..
 cp app/parameters.toml /data/PG_containers/conf/PG_prod/parameters.toml
